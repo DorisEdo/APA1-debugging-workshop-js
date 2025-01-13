2290f19e2f6f1084d982545b556afc86708b70f1
DorisEdo <167077232+DorisEdo@users.noreply.github.com>
2025-01-13 12:47:21 +0000

    Test for capitalize function 3

--- 03. capitalise/solution.js
+++ 03. capitalise/solution.js
@@ -1,7 +1,8 @@
 function capitalise(word) {
-  word[0] = word[0].toUpperCase();
-  return word;
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
+  
 }
+console.log(capitalise("jumper"));
 
 module.exports = { capitalise };
 
