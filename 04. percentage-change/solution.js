d163f0aa33fd2cb3f976d51d5e801e25572b2953
DorisEdo <167077232+DorisEdo@users.noreply.github.com>
2025-01-13 14:05:20 +0000

    Test for percentage change

--- 04. percentage-change/solution.js
+++ 04. percentage-change/solution.js
@@ -1,8 +1,11 @@
 function calculatePercentageChange(original, newAmount) {
+  if(original === 0){
+    return newAmount * 100;
+  }
   const difference = Math.abs(newAmount - original);
-  const average = (original - newAmount) / 2;
-  const percentageDifference = (average / difference) * 100;
-  return percentageDifference.toFixed(2);
+  const percentageDifference = (difference/ original) * 100;
+  return parseFloat(percentageDifference.toFixed(2));
 }
+console.log(calculatePercentageChange(100, 120));
 
 module.exports = { calculatePercentageChange };
